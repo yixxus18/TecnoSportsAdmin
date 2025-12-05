78f1fff813da3ba91be69ccf206ac34310501136
Ricardo Ramón Sánchez González <22170038@uttcampus.edu.mx>
2025-12-04 22:31:58 -0600

    Usersmodule import

--- src/api/favorites/favorites.module.ts
+++ src/api/favorites/favorites.module.ts
@@ -3,11 +3,13 @@ import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Favorite } from './entities/favorite.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     // 👈 1. Usa forFeature para registrar la entidad y su repositorio
     TypeOrmModule.forFeature([Favorite]),
+    UsersModule,
   ],
   controllers: [FavoritesController],
   providers: [FavoritesService],
