0f0a3dc5f32e1e19d0537022b7dadffa49aaead9
Ricardo Ramón Sánchez González <22170038@uttcampus.edu.mx>
2025-12-04 22:30:06 -0600

    Notifications import matches

--- src/api/matches/matches.module.ts
+++ src/api/matches/matches.module.ts
@@ -4,9 +4,13 @@ import { MatchesController } from './matches.controller';
 import { Match } from './entities/match.entity';
 import { Team } from '../teams/entities/team.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationSubscriptionModule } from 'src/notification-subscription/notification-subscription.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Match, Team])],
+  imports: [
+    TypeOrmModule.forFeature([Match, Team]),
+    NotificationSubscriptionModule,
+  ],
   controllers: [MatchesController],
   providers: [MatchesService],
   exports: [MatchesService],
