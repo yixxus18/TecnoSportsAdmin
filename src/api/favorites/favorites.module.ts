75e7a306382ae15e07e93fe14507c41f678b0a4a
Ricardo Ramón Sánchez González <22170038@uttcampus.edu.mx>
2025-12-04 22:19:07 -0600

    Fix Favorite repository

--- src/api/favorites/favorites.module.ts
+++ src/api/favorites/favorites.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Favorite } from './entities/favorite.entity';
 
 @Module({
+  imports: [
+    // 👈 1. Usa forFeature para registrar la entidad y su repositorio
+    TypeOrmModule.forFeature([Favorite]),
+  ],
   controllers: [FavoritesController],
   providers: [FavoritesService],
 })
