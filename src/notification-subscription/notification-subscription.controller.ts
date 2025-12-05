27a9d089ede27c608b8c0fabe326c189efd4db50
Ricardo Ramón Sánchez González <22170038@uttcampus.edu.mx>
2025-12-04 22:06:20 -0600

    Notificaciones Vapid

--- src/notification-subscription/notification-subscription.controller.ts
+++ src/notification-subscription/notification-subscription.controller.ts
@@ -1,7 +1,9 @@
 import { Controller } from '@nestjs/common';
-import { NotificationSubscriptionService } from './notification-subscription.service';
+import { NotificationsService } from './notification-subscription.service';
 
 @Controller('notification-subscription')
 export class NotificationSubscriptionController {
-  constructor(private readonly notificationSubscriptionService: NotificationSubscriptionService) {}
+  constructor(
+    private readonly notificationSubscriptionService: NotificationsService,
+  ) {}
 }
